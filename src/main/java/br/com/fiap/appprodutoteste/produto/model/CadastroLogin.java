69e4d57cc62dc2d1b6e131cad1a667c43bc1f03c
Isadora <rm84246@fiap.com.br>
2022-04-30 09:27:05 -0300

    Melhorias

---
+++ src/main/java/br/com/fiap/appprodutoteste/produto/model/CadastroLogin.java
@@ -0,0 +1,70 @@
+package br.com.fiap.appprodutoteste.produto.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CadastroLogin {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	@Column(nullable = false)
+	private String email; 
+	private String primeiroNome; 
+	private String segundoNome;
+    private String senha;
+	
+	public CadastroLogin() {
+		
+	}
+	
+	public CadastroLogin(Long id, String email, String primeiroNome, String segundoNome, String senha) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.primeiroNome = primeiroNome;
+		this.segundoNome = segundoNome;
+        this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPrimeiroNome() {
+		return this.primeiroNome;
+	}
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSegundoNome() {
+		return this.segundoNome;
+	}
+	public void setSegundoNome(String segundoNome) {
+		this.segundoNome = segundoNome;
+	}
+
+    public String getSenha() {
+		return this.senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
